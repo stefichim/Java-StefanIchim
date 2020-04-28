02f83c205e235ffa57c13e4e212e299288b60fa1
stefichim <ichim.stefan99@gmail.com>
2020-04-28 16:31:21 +0300

    server-client Connection

---
+++ lab10Comp/ServerApplication/src/main/java/GameServer.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameServer {
+
+	// Define the port on which the server is listening
+	public static final int PORT = 8101;
+
+	public GameServer() throws IOException {
+		ServerSocket serverSocket = null;
+		try {
+
+			serverSocket = new ServerSocket(PORT);
+			while (true) {
+				System.out.println("Waiting for a client ...");
+				Socket socket = serverSocket.accept();
+				// Execute the client's request in a new thread
+				new ClientThread(socket).start();
+			}
+		} catch (IOException e) {
+			System.err.println("Ooops... " + e);
+		} finally {
+			serverSocket.close();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		GameServer server = new GameServer();
+	}
+}
