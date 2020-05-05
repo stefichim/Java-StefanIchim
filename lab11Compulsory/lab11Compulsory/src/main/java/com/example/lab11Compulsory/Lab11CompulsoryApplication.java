83e99a6b7670a9e41b6f247a1e553fa6894b10ea
stefichim <ichim.stefan99@gmail.com>
2020-05-05 18:32:41 +0300

    lab11_compWithSpringAdded

---
+++ lab11Compulsory/lab11Compulsory/src/main/java/com/example/lab11Compulsory/Lab11CompulsoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab11Compulsory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11CompulsoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab11CompulsoryApplication.class, args);
+	}
+
+}
---
+++ lab11Compulsory/lab11Compulsory/src/main/java/com/example/lab11Compulsory/Player.java
@@ -0,0 +1,5 @@
+package com.example.lab11Compulsory;
+
+public class Player {
+
+}
